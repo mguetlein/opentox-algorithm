7fb029d347bf15fce65950bd1b0d908cb203c35a
Christoph Helma <helma@in-silico.de>
2009-12-19 13:32:53 +0100

    OWL-DL fixed for dataset and fminer

--- fminer.rb
+++ fminer.rb
@@ -17,27 +17,28 @@ post '/fminer/?' do
 		feature_uri = params[:feature_uri] or halt 404, "Please submit a feature_uri parameter."
 		training_dataset = OpenTox::Dataset.find params[:dataset_uri]
 		feature_dataset = OpenTox::Dataset.new
-		feature_dataset.title = "BBRC representatives for " + training_dataset.title
+		title = "BBRC representatives for " + training_dataset.title
+		feature_dataset.title = title
 		feature_dataset.source = url_for('/fminer',:full)
-		bbrc_feature = feature_dataset.find_or_create_feature(:name => "BBRC representative", :source => url_for("/fminer",:full))
+		bbrc_uri = url_for("/fminer#BBRC_representative",:full)
+		bbrc_feature = feature_dataset.find_or_create_feature bbrc_uri
 
 		id = 1 # fminer start id is not 0
 		compounds = []
-		#training_dataset.data_entries.each do |c,f|
 		training_dataset.feature_values(feature_uri).each do |c,f|
 			smiles = OpenTox::Compound.new(:uri => c.to_s).smiles
 			compound = feature_dataset.find_or_create_compound(c.to_s)
 			puts "No #{feature_uri} for #{c.to_s}." if f.size == 0
 			f.each do |act|
-				puts act
+				#puts act
 				case act.to_s
 				when "true"
-					puts smiles + "\t" + true.to_s
+					#puts smiles + "\t" + true.to_s
 					compounds[id] = compound
 					@@fminer.AddCompound(smiles,id)
 					@@fminer.AddActivity(true, id)
 				when "false"
-					puts smiles + "\t" + false.to_s
+					#puts smiles + "\t" + false.to_s
 					compounds[id] = compound
 					@@fminer.AddCompound(smiles,id)
 					@@fminer.AddActivity(false, id)
@@ -62,11 +63,10 @@ post '/fminer/?' do
 				else
 					effect = 'deactivating'
 				end
-				#tuple = feature_dataset.find_or_create_tuple( :smarts => smarts, :p_value => p_value, :effect => effect )
-				tuple = feature_dataset.create_tuple( :smarts => smarts, :p_value => p_value, :effect => effect )
+				tuple = feature_dataset.create_tuple(bbrc_feature,{ url_for('/fminer#smarts',:full) => smarts, url_for('/fminer#p_value',:full) => p_value, url_for('/fminer#effect',:full) => effect })
 				#puts "#{f[0]}\t#{f[1]}\t#{effect}"
 				ids.each do |id|
-					feature_dataset.add_data_entry compounds[id], bbrc_feature, tuple
+					feature_dataset.add_tuple compounds[id], tuple
 				end
 			end
 		end
